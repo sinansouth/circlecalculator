6fdc6c12424db2730b8870be73cc4663bb1285c0
sinansouth <64125976+sinansouth@users.noreply.github.com>
2023-07-27 20:28:29 +0300

    kapaligazbasinci.c

--- kapaligazbasinci.c
+++ kapaligazbasinci.c
@@ -3,9 +3,21 @@
 
 int main()
 {
-//fatura ücreti=harcanan elektrik*birim fiyat+abone ücreti
 
+    float pressure,constantR
+    int moles,volume,heat
+    constantR=0.82;
 
+    printf("Kabin hacmini girin:");
+    scanf("%f",&volume);
+    printf("\nKaptaki mol sayisini girin:");
+    scanf("%d",&moles);
+    printf("\nOrtam sicakligini girin:");
+    scanf("%d",&heat);
+
+    pressure=(moles*constantR*heat)/volume;
+
+    printf("%d hacimli kaptaki gaz basinci:%f\n",volume,pressure);
 
     return 0;
 }
